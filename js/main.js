5a482922c787952d90d57174831927926fbf1030
Helen Guo <helengreen2008@gmail.com>
2018-08-30 19:44:35 +1000

    refactor: Separate display card function from click event listener

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 
-// display icon when a card is clicked
-
+// A list that holds all cards
 const card = document.getElementsByClassName('card');
 // getElementsByClassName() returns array-like HTMLCollection / NodeList instead of an array, array-like object inherits object.prototype instead of Array.prototype.
 // creat cards array
 const cards = [...card];
-for (const card of cards)
-    card.addEventListener('click', function(){
-    this.setAttribute('style', 'background: #02b3e4; font-size: 33px;');
-})
 
+// display the clicked cards' symbol
+let displayCard = function(){
+    this.setAttribute('style', 'background: #02b3e4; font-size: 33px; cursor: default;');
+}
+
+// set up the event listener for a card
+for (const card of cards){
+    card.addEventListener('click', displayCard);
+};
