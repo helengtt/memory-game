e23186bbe63d2649aa09b5e11944b33ba9fad8f4
Helen Guo <helengreen2008@gmail.com>
2018-08-30 21:09:16 +1000

    refactor: Create a card list in Javascript

--- js/main.js
+++ js/main.js
@@ -1,5 +1,57 @@
-
-// A list that holds all cards
+/*
+ * Create a list that holds all cards
+ */
+const cardElement = document.querySelector('.deck');
+cardElement.innerHTML =`
+    <li class="card">
+        <i class="fa fa-diamond"></i>
+    </li>
+    <li class="card">
+        <i class="fa fa-paper-plane-o"></i>
+    </li>
+    <li class="card">
+        <i class="fa fa-anchor"></i>
+    </li>
+    <li class="card">
+        <i class="fa fa-bolt"></i>
+    </li>
+    <li class="card">
+        <i class="fa fa-cube"></i>
+    </li>
+    <li class="card">
+        <i class="fa fa-anchor"></i>
+    </li>
+    <li class="card">
+        <i class="fa fa-leaf"></i>
+    </li>
+    <li class="card">
+        <i class="fa fa-bicycle"></i>
+    </li>
+    <li class="card">
+        <i class="fa fa-diamond"></i>
+    </li>
+    <li class="card">
+        <i class="fa fa-bomb"></i>
+    </li>
+    <li class="card">
+        <i class="fa fa-leaf"></i>
+    </li>
+    <li class="card">
+        <i class="fa fa-bomb"></i>
+    </li>
+    <li class="card">
+        <i class="fa fa-bolt"></i>
+    </li>
+    <li class="card">
+        <i class="fa fa-bicycle"></i>
+    </li>
+    <li class="card">
+        <i class="fa fa-paper-plane-o"></i>
+    </li>
+    <li class="card">
+        <i class="fa fa-cube"></i>
+    </li>
+`;
 const card = document.getElementsByClassName('card');
 // getElementsByClassName() returns array-like HTMLCollection / NodeList instead of an array, array-like object inherits object.prototype instead of Array.prototype.
 // creat cards array
@@ -10,7 +62,7 @@ let displayCard = function(){
     this.setAttribute('style', 'background: #02b3e4; font-size: 33px; cursor: default;');
 }
 
-// set up the event listener for a card
+// set up the click event listener for a card
 for (const card of cards){
     card.addEventListener('click', displayCard);
 };
