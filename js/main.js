682dd771d32c1e2d02e82a22e6e5fdef4d6871e1
Helen Guo <helengreen2008@gmail.com>
2018-09-04 12:12:43 +1000

    feat: Match the clicked cards and show the matched or unmatched results.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 /*
  * Create a list that holds all card symbols
  */
-const symbols = ['fa fa-diamond', 'fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-bolt', 'fa fa-cube', 'fa fa-cube', 'fa fa-anchor', 'fa fa-anchor', 'fa fa-leaf', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bicycle'];
+const symbols = ['fa fa-diamond', 'fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-bolt', 'fa fa-cube', 'fa fa-cube', 'fa fa-bomb', 'fa fa-bomb', 'fa fa-leaf', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bicycle'];
 
 /*
  * Display the cards on the page
  */
 
-// - shuffle cards (Shuffle function from http://stackoverflow.com/a/2450976)
+// shuffle cards (Shuffle function from http://stackoverflow.com/a/2450976)
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -22,11 +22,11 @@ function shuffle(array) {
     return array;
 }
 
-//  - loop through each card and create its HTML
-//  - add each card's HTML to the page
+// loop through each card and create its HTML
 let symbolList = shuffle(symbols);
 function creatCard(){
     const deck = document.querySelector('.deck');
+    // add each card's HTML to the page
     for (const symbol of symbolList){
         const cardHtmlText = `<li class="card"><i class="${symbol}"></i></li>`;
         deck.insertAdjacentHTML('beforeend', cardHtmlText);
@@ -35,14 +35,49 @@ function creatCard(){
 
 window.onload = creatCard();
 
-// display the clicked cards
-let displayCard = function(){
-    this.setAttribute('style', 'background: #02b3e4; font-size: 33px; cursor: default;');
-}
-
 // set up the click event listener for a card
 const cardElement = document.getElementsByClassName('card');
 const cards = [...cardElement];
+
 for (const card of cards){
     card.addEventListener('click', displayCard);
+}
+
+// display the clicked cards
+// add the card to a *list* of "open" cards
+let openedCards = [];
+function displayCard(){
+    this.classList.add('open');
+    openedCards.push(this);
+    matchCard();
+}
+
+// check to see if the two cards match
+function matchCard(){
+    if (openedCards.length === 2) { 
+        if (openedCards[0].firstChild.className === openedCards[1].firstChild.className) {
+            matched();
+        } else {
+            unmatched();
+        }
+    }
+}
+
+// if the cards do match, lock the cards
+function matched(){
+    openedCards[0].classList.add('match');
+    openedCards[1].classList.add('match');
+    openedCards[0].classList.remove('open');
+    openedCards[1].classList.remove('open');
+    openedCards = [];
+}
+
+// if the cards do not match, hide the cards
+function unmatched(){
+    setTimeout (function(){
+        openedCards[0].classList.remove('open');
+        openedCards[1].classList.remove('open');
+        openedCards = [];
+    },1000);
+    
 }
